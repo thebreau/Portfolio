77ce0184441e361c3133bdd3b81b88ceb57f9d42
Ethan Breau <000906500@mohawkcollege.ca>
2024-01-15 11:03:56 -0500

    commit of initial projects for employer review.

---
+++ Travel Companion App/App.js
@@ -0,0 +1,312 @@
+/* StAuth10244: I Ethan Breau, 000906500 certify that this material is my original work. 
+No other person's work has been used without due acknowledgement. I have not made my work available to anyone else. */
+
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Text,
+  View,
+  Modal,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import axios from "axios";
+import PagerView from "react-native-pager-view";
+
+export default function App() {
+  // State hook for current region
+  const [region, setRegion] = useState(null);
+
+  // Effect hook to request location permission and get current location on mount
+  useEffect(() => {
+    // Async function to handle location access and setting region
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+  // State hooks for various features like points of interest, modal visibility, etc.
+  const [pois, setPois] = useState([]);
+  const [selectedPoiType, setSelectedPoiType] = useState("restaurant");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [myLocation, setMyLocation] = useState(null);
+
+  // Similar effect hook to the above, but it also fetches Places of Interest (POIs) after updating the region
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+
+      try {
+        let location = await Location.getCurrentPositionAsync({});
+        const newRegion = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        };
+        setRegion(newRegion);
+
+        // Fetch POIs after updating the region
+        fetchPOIs(
+          newRegion.latitude,
+          newRegion.longitude,
+          selectedPoiType
+        ).then(setPois);
+      } catch (error) {
+        console.error("Failed to get current location: ", error);
+      }
+    })();
+  }, []);
+
+  // Function to fetch points of interest (POIs) from Google Places API
+  const fetchPOIs = async (latitude, longitude, type) => {
+    const apiKey = "AIzaSyD-rAlywAaoHswuuR2V-v9F4Jt278AiTEA";
+    const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&type=${type}&key=${apiKey}`;
+
+    try {
+      const response = await axios.get(url);
+      console.log("API response: ", response.data);
+      return response.data.results.map((poi) => ({
+        ...poi,
+        photo_reference: poi.photos?.[0]?.photo_reference,
+      }));
+    } catch (error) {
+      console.error("Error fetching POIs: ", error);
+      return [];
+    }
+  };
+
+  // Function to handle POI type selection and update POIs based on the type
+  const handleSelectPoiType = (type) => {
+    setSelectedPoiType(type);
+    fetchPOIs(region.latitude, region.longitude, type).then(setPois);
+    setModalVisible(false);
+  };
+
+  // Function to update the user's current location
+  const findMyLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission to access location was denied");
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    setMyLocation({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+  };
+
+  return (
+    // SafeAreaView to ensure the app UI renders correctly on all devices
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* Modal for selecting POI types */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={{ flex: 1, justifyContent: "center", marginTop: 350 }}>
+          <View>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#007BFF",
+                padding: 10,
+                alignItems: "center",
+                marginHorizontal: 2,
+              }}
+              onPress={() => handleSelectPoiType("restaurant")}
+            >
+              <Text style={{ color: "#FFFFFF" }}>Restaurants</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                marginTop: 10,
+                marginHorizontal: 2,
+                backgroundColor: "#007BFF",
+                padding: 10,
+                alignItems: "center",
+              }}
+              onPress={() => handleSelectPoiType("museum")}
+            >
+              <Text style={{ color: "#FFFFFF" }}>Museums</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                marginTop: 10,
+                marginHorizontal: 2,
+                backgroundColor: "#007BFF",
+                padding: 10,
+                alignItems: "center",
+              }}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={{ color: "#FFFFFF" }}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      {/* Modal for displaying selected photo */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={!!selectedPhoto}
+        onRequestClose={() => setSelectedPhoto(null)}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0,0,0,0.5)",
+          }}
+        >
+          {selectedPhoto && (
+            <Image
+              style={{ width: 300, height: 300 }}
+              source={{
+                uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${selectedPhoto}&key=AIzaSyD-rAlywAaoHswuuR2V-v9F4Jt278AiTEA`,
+              }}
+            />
+          )}
+          <TouchableOpacity
+            style={{
+              backgroundColor: "#007BFF",
+              padding: 10,
+              alignItems: "center",
+              borderRadius: 5,
+              marginTop: 5,
+              width: 300,
+            }}
+            onPress={() => setSelectedPhoto(null)}
+          >
+            <Text style={{ color: "#FFFFFF" }}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      {/* Main view for the map and POI list */}
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        {/* MapView showing current region and markers for POIs */}
+        {region && (
+          <MapView style={{ flex: 1 }} region={region}>
+            {pois.map((poi) => (
+              <Marker
+                key={poi.place_id}
+                coordinate={{
+                  latitude: poi.geometry.location.lat,
+                  longitude: poi.geometry.location.lng,
+                }}
+                title={poi.name}
+              />
+            ))}
+            {myLocation && (
+              <Marker
+                coordinate={myLocation}
+                title="My Location"
+                pinColor="blue"
+              />
+            )}
+          </MapView>
+        )}
+        {/* PagerView for swiping through POIs */}
+        <PagerView
+          style={{
+            flex: 0.1,
+            marginTop: "auto",
+            backgroundColor: "lightgrey",
+          }}
+          initialPage={0}
+        >
+          {pois.map((poi, index) => (
+            <View
+              key={index}
+              style={{
+                padding: 10,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <View>
+                  <Text>{poi.name}</Text>
+                  <Text>{poi.vicinity}</Text>
+                </View>
+                {poi.photo_reference && (
+                  <TouchableOpacity
+                    onPress={() => setSelectedPhoto(poi.photo_reference)}
+                  >
+                    <Image
+                      style={{ width: 50, height: 50, marginLeft: 10 }}
+                      source={{
+                        uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${poi.photo_reference}&key=AIzaSyD-rAlywAaoHswuuR2V-v9F4Jt278AiTEA`,
+                      }}
+                    />
+                  </TouchableOpacity>
+                )}
+              </View>
+            </View>
+          ))}
+        </PagerView>
+        {/* Buttons for finding user location and selecting POIs */}
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#007BFF",
+            padding: 10,
+            alignItems: "center",
+            marginTop: 2,
+          }}
+          onPress={findMyLocation}
+        >
+          <Text style={{ color: "#FFFFFF" }}>Find My Location</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            marginTop: 2,
+            backgroundColor: "#007BFF",
+            padding: 10,
+            alignItems: "center",
+          }}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={{ color: "#FFFFFF" }}>Select Places of Interest</Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
---
+++ WoW Auction House App/App.js
@@ -0,0 +1,262 @@
+// StAuth10244: I Ethan Breau, 000906500 certify that this material is my original work.
+// No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
+import base64 from "react-native-base64";
+
+// Main component of the app
+export default function App() {
+  // Converts copper currency to gold, silver, and copper
+  const convertCurrency = (copper) => {
+    const gold = Math.floor(copper / 10000);
+    const silver = Math.floor((copper % 10000) / 100);
+    const remainingCopper = copper % 100;
+
+    return `${gold}g ${silver}s ${remainingCopper}c`;
+  };
+
+  // Component to display each auction item
+  const AuctionItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      {/* Item details */}
+      <View style={styles.leftContainer}>
+        <Text style={{ color: "white", fontWeight: "bold" }}>
+          Item ID: {item.item.id}
+        </Text>
+        <Text style={{ color: "white", fontWeight: "bold", marginBottom: 20 }}>
+          Quantity: {item.quantity}
+        </Text>
+        <Text style={{ color: "white", fontWeight: "bold" }}>
+          Time Left: {item.time_left}
+        </Text>
+      </View>
+      {/* Bid and Buyout buttons */}
+      <View style={styles.rightContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            Bid: {convertCurrency(item.bid)}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            Buyout: {convertCurrency(item.buyout)}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
+  // Blizzard API credentials
+  const clientId = "9de927a97fed4bfa9f3cb65b27f14bc4";
+  const clientSecret = "RUUVNjdSQFB6MH5jI7DeKUVoRJ48nVPs";
+
+  // State hooks for auction data, filtering, and auction house selection
+  const [auctionData, setAuctionData] = useState([]);
+  const [filteredAuctionData, setFilteredAuctionData] = useState([]);
+  const [itemId, setItemId] = useState("");
+  const [auctionHouse, setAuctionHouse] = useState(2); // Default to Alliance
+
+  // Sets auction house and fetches data
+  const setAuctionHouseAndFetch = (houseValue) => {
+    setFilteredAuctionData([]);
+    setAuctionHouse(houseValue);
+    fetchAuctions();
+  };
+
+  // Fetch auctions on component mount and when auctionHouse changes
+  useEffect(() => {
+    fetchAuctions();
+  }, [auctionHouse]);
+
+  // Fetches auctions from the Blizzard API
+  const fetchAuctions = async () => {
+    setFilteredAuctionData([]);
+    const accessToken = await fetchAccessToken(clientId, clientSecret);
+    try {
+      const response = await axios.get(
+        `https://us.api.blizzard.com/data/wow/connected-realm/4395/auctions/${auctionHouse}?namespace=dynamic-classic-us&locale=en_US&access_token=${accessToken}`
+      );
+      setAuctionData(response.data.auctions);
+    } catch (error) {
+      console.error("Error fetching auctions:", error);
+    }
+  };
+
+  // Fetches access token for Blizzard API
+  const fetchAccessToken = async () => {
+    try {
+      const authHeader = base64.encode(`${clientId}:${clientSecret}`);
+      const response = await axios.post(
+        "https://oauth.battle.net/token",
+        "grant_type=client_credentials",
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: `Basic ${authHeader}`,
+          },
+        }
+      );
+      return response.data.access_token;
+    } catch (error) {
+      console.error("Error fetching access token:", error);
+      throw error;
+    }
+  };
+
+  // Filters auctions by item ID
+  const fetchAuctionsByItemId = () => {
+    const filteredData = auctionData.filter(
+      (auction) => auction.item.id.toString() === itemId
+    );
+    setFilteredAuctionData(filteredData);
+  };
+
+  // Button component for selecting auction house
+  const AuctionHouseButton = ({ title, houseValue, currentHouse }) => (
+    <TouchableOpacity
+      style={[
+        styles.auctionButton,
+        {
+          backgroundColor: houseValue === currentHouse ? "#0074e0" : "#23252b",
+        },
+      ]}
+      onPress={() => setAuctionHouse(houseValue)}
+    >
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+
+  // Main rendering of the app interface
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Warcraft image display */}
+      <Image
+        source={require("./assets/warcraft.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      {/* Auction house selection buttons */}
+      <View style={styles.buttonGroup}>
+        <AuctionHouseButton
+          title="Alliance"
+          houseValue={2}
+          currentHouse={auctionHouse}
+          onPress={() => setAuctionHouseAndFetch(2)}
+        />
+        <AuctionHouseButton
+          title="Horde"
+          houseValue={6}
+          currentHouse={auctionHouse}
+          onPress={() => setAuctionHouseAndFetch(6)}
+        />
+        <AuctionHouseButton
+          title="Neutral"
+          houseValue={7}
+          currentHouse={auctionHouse}
+          onPress={() => setAuctionHouseAndFetch(7)}
+        />
+      </View>
+      {/* Refresh auctions button */}
+      <TouchableOpacity style={styles.refreshButton} onPress={fetchAuctions}>
+        <Text style={styles.buttonText}>Refresh Auctions</Text>
+      </TouchableOpacity>
+      {/* Item ID input field */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Item ID"
+        placeholderTextColor={"white"}
+        value={itemId}
+        onChangeText={setItemId}
+        onSubmitEditing={fetchAuctionsByItemId}
+      />
+
+      {/* Auction items list */}
+      <FlatList
+        data={
+          filteredAuctionData.length > 0 ? filteredAuctionData : auctionData
+        }
+        renderItem={({ item }) => <AuctionItem item={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        style={styles.list}
+      />
+    </SafeAreaView>
+  );
+}
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    padding: 20,
+    backgroundColor: "#15171e",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    padding: 10,
+    marginVertical: 8,
+    backgroundColor: "#23252b",
+    borderRadius: 5,
+  },
+  leftContainer: {
+    flex: 1,
+    marginTop: 8,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  list: {
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "#0074e0",
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  image: {
+    width: 400,
+    height: 150,
+    alignSelf: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "#0074e0",
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10,
+    marginVertical: 10,
+    color: "white",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+  },
+  auctionButton: {
+    backgroundColor: "#0074e0",
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 2,
+    marginBottom: 10,
+  },
+  refreshButton: {
+    backgroundColor: "#0074e0",
+    padding: 10,
+    borderRadius: 5,
+  },
+});
